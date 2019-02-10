61098b7a50d40330ede7139e4ffe890de53fc0d4
Manne Laukkanen <manne@smartdatahub.io>
2019-02-10 13:29:07 +0200

    Logistic regression with NumPy notebook for the call.

--- springboard/datacamp/3_1_python_for_data_science/3_Lambda.py
+++ springboard/datacamp/3_1_python_for_data_science/3_Lambda.py
@@ -1,10 +1,17 @@
-# Lambdas
+##############################################################
+# LAMBDAS
+##############################################################
+
 # Example:
 raise_to_power = lambda x, y: x**y 
 # params,then what you do with them AND what it returns. 
 # Note how much space is saved.
 
 nums = [48,6,9,21,1}
+
+##############################################################
+# MAP 
+##############################################################
 # Useful, with e.g. map(function, sequence)
 square_all = map(lambda num: num ** 2, nums)
 # so, map does a for each with a function and what ever you gave.
@@ -27,8 +34,7 @@ def echo_word(word1, echo):
 # ..to lambda 
 echo_word = (lambda word1, echo: word1 * echo)
 
-# then example with map
-# map(function, iterable) applies a function over an object, such as a list.
+# then example with map(function, iterable) applies a function over an object, such as a list...so e.g. a lambda-function..
 # ...becomes sturdy with lambdas, since without lambda, the defs put in map might end up taxing the working memory of the programmer. Now fits on one line sometimes maybe.
 
 
@@ -44,3 +50,44 @@ shout_spells_list = list(shout_spells)
 
 # Convert shout_spells into a list and print it
 print(shout_spells_list)
+
+##############################################################
+# using lambda with FILTER (returns BOOLEAN)
+##############################################################
+# generally 
+number_list = range(-5, 5)
+less_than_zero = list(filter(lambda x: x < 0, number_list))
+print(less_than_zero)
+
+# Output: [-5, -4, -3, -2, -1]
+
+# So, in short, how to cut some data out based on length.
+# Create a list of strings: fellowship
+fellowship = ['frodo', 'samwise', 'merry', 'pippin', 'aragorn', 'boromir', 'legolas', 'gimli', 'gandalf']
+
+# Use filter() to apply a lambda function over fellowship: result
+result = filter(lambda member: len(member) > 6, fellowship)
+
+# Convert result to a list: result_list
+result_list = list(result)
+
+# Convert result into a list and print it
+print(result_list)
+
+
+##############################################################
+# REDUCE - returns one item
+##############################################################
+
+# Import reduce from functools
+from functools import reduce 
+
+# Create a list of strings: stark
+stark = ['robb', 'sansa', 'arya', 'brandon', 'rickon']
+
+# Use reduce() to apply a lambda function over stark: result
+result = reduce(lambda item1, item2: item1 + item2, stark)
+
+# Print the result
+print(result)
+## 3n t4y51n t4junnu+ m1k51 +u733 yk51 +u705
--- src/dataframe_regression_filler.py
+++ src/dataframe_regression_filler.py
@@ -8,17 +8,32 @@ column of said dataframe to fill with products of said regression model.
 ..returns a dataframe, with values filled.
 
 """
+# Rather import these in the program itself?
 import pandas as pd
 import statsmodels.api as sm
 
-    predictors = pd.DataFrame(dataframe, columns = [*args])
+    try:
+        predictors = pd.DataFrame(dataframe, columns = [*args])
+    except:
+        print('You sure you are predicting with columns that actually exist in the dataframe? Here are the columns in the dataframe')
+        list(dataframe) # if does not work as expected, just embed the list(df) in a print()
     target_column = column
     dataframe_to_be_filled = dataframe
 
     model = sm.OLS(target_column,predictors).fit()
-    # .... what here? - how do I overwrite the dataframe?
+    # .... what here?
+        # Should I handle the whole dataframe or just a subset of vars relevant to the exercise 
+            # Subset
+        # should I overwrite the dataframe inplace - or create a copy?
+            # Make a copy and handle that
+            # Actually NumPy:s handled often
+        # how do I overwrite the dataframe - does the "inplace" really work?
+            # 1) value X := Y in the original one (inplace)
+            # 2) -> to the new dataframe via a calculated NumPy array -> calculate a new column with the same name with the new values 
+        
+            # how do I overwrite the dataframe - does the "inplace" really work?
     # predictions = model.predict(X)
     
     DataFrame.replace(to_replace=target_column, value=model.OUTPUT, inplace=True, limit=None, regex=False, method='pad')
     
-    return filled_dataframe 
+    return filled_dataframe # this actually will be a new one, calculated on the basis of the old.
